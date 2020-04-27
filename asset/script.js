de04f6143c211c314a983d9ed961811e19ee45dc
HeldiTioPratama <pratamahelditio@gmail.com>
2020-04-27 18:32:09 +0700

    menambahkan web ke halaman github

---
+++ asset/script.js
@@ -0,0 +1,7 @@
+function ubahWarna() {
+    let warnaBg = hexabg.value;
+    let warnaText = hexatext.value
+    document.body.style.background = warnaBg;
+    document.body.style.color = warnaText;
+}
+
